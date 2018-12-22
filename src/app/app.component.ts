b13d21fc53852b4d0fa5ae6bc82326a556e999f1
Chandler Fang <chandlerfang@gmail.com>
2018-12-22 20:32:53 +1100

    14-destroy-dynamic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef, ViewChild, ComponentFactoryResolver, AfterContentInit } from '@angular/core';
+import { Component, ViewContainerRef, ComponentRef, ViewChild, ComponentFactoryResolver, AfterContentInit } from '@angular/core';
 
 import { AuthFormComponent } from './auth-form//auth-form.component';
 
@@ -8,26 +8,33 @@ import { User } from './auth-form/auth-form.interface';
   selector: 'app-root',
   template: `
     <div>
+    <button (click)="destoryComponent()">
+      Destory
+    </button>
       <div #entry></div>
     </div>
   `
 })
 export class AppComponent implements AfterContentInit {
   @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-
+  component: ComponentRef<AuthFormComponent>;
   constructor(
     private resolver: ComponentFactoryResolver
   ) {}
 
   ngAfterContentInit() {
     const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-    const component = this.entry.createComponent(authFormFactory);
-    console.log(component.instance);
-    component.instance.title = 'Create account';
-    component.instance.submitted.subscribe(this.loginUser);
+    this.component = this.entry.createComponent(authFormFactory);
+    console.log(this.component.instance);
+    this.component.instance.title = 'Create account';
+    this.component.instance.submitted.subscribe(this.loginUser);
   }
 
   loginUser(user: User) {
     console.log('Login', user);
+  }
+
+  destoryComponent() {
+    this.component.destroy();
   }
 }
