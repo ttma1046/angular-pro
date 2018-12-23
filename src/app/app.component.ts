2cc0aa5a2bf46d27d6caa6311a0fbd0dba0493a0
Chandler Fang <chandlerfang@gmail.com>
2018-12-23 15:03:11 +1100

    16-template-viewcontainerref

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewContainerRef, ComponentRef, ViewChild, ComponentFactoryResolver, AfterContentInit } from '@angular/core';
+import { Component, TemplateRef, ViewContainerRef, ComponentRef, ViewChild, ComponentFactoryResolver, AfterContentInit } from '@angular/core';
 
 import { AuthFormComponent } from './auth-form//auth-form.component';
 
@@ -8,42 +8,21 @@ import { User } from './auth-form/auth-form.interface';
     selector: 'app-root',
     template: `
     <div>
-    <button (click)="destoryComponent()">
-      Destory
-    </button>
-    <button (click)="MoveComponent()">
-      Move Component
-    </button>
       <div #entry></div>
+      <ng-template #tmpl>
+        Chandle Fang : Sydney, AU
+      </ng-template>
     </div>
   `
 })
 export class AppComponent implements AfterContentInit {
     @ViewChild('entry', { read: ViewContainerRef }) entry: ViewContainerRef;
-    component: ComponentRef<AuthFormComponent>;
+    @ViewChild('tmpl') tmpl: TemplateRef<any>;
     constructor(
         private resolver: ComponentFactoryResolver
     ) { }
 
     ngAfterContentInit() {
-        const authFormFactory = this.resolver.resolveComponentFactory(AuthFormComponent);
-        this.entry.createComponent(authFormFactory);
-
-        this.component = this.entry.createComponent(authFormFactory, 0);
-        console.log(this.component.instance);
-        this.component.instance.title = 'Create account';
-        this.component.instance.submitted.subscribe(this.loginUser);
-    }
-
-    loginUser(user: User) {
-        console.log('Login', user);
-    }
-
-    destoryComponent() {
-        this.component.destroy();
-    }
-
-    MoveComponent() {
-      this.entry.move(this.component.hostView, 1);
+      this.entry.createEmbeddedView(this.tmpl);
     }
 }
