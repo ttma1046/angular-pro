72fd4a25ec390fd2191856a74ed96a751372948d
Chandler Fang <chandlerfang@gmail.com>
2018-12-23 23:35:57 +1100

    23-hostlistener

--- src/app/credit-card/credit-card.directive.ts
+++ src/app/credit-card/credit-card.directive.ts
@@ -1,12 +1,27 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, HostListener, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[credit-card]'
+    selector: '[credit-card]'
 })
 export class CreditCardDirective {
+    @HostListener('input', ['$event'])
+    onKeyDown(event: KeyboardEvent) {
+        const input = event.target as HTMLInputElement;
 
-  constructor(private element: ElementRef) {
-      console.log(this.element);
-  }
+        let trimmed = input.value.replace(/s+/g, '');
+        if (trimmed.length  > 16) {
+            trimmed = trimmed.substr(0, 16);
+        }
+
+        let numbers  = [];
+        for (let i = 0; i < trimmed.length; i += 4) {
+            numbers.push(trimmed.substr(i, 4));
+        }
+        console.log(numbers);
+        input.value = numbers.join(' ');
+    }
+
+    constructor(private element: ElementRef) {
+    }
 
 }
