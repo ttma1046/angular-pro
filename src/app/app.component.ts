da46083de6ee051a09cb0259cbf59c6bec957861
Chandler Fang <chandlerfang@gmail.com>
2018-12-23 22:30:39 +1100

    20-viewencapsulation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,12 @@
-import {
-  Component,
-  TemplateRef, ViewContainerRef, ComponentRef, ViewChild, ComponentFactoryResolver, AfterContentInit } from '@angular/core';
-
-import { AuthFormComponent } from './auth-form//auth-form.component';
-
-import { User } from './auth-form/auth-form.interface';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-root',
-    template: `
+  selector: 'app-root',
+  template: `
     <div>
-      <ng-container [ngTemplateOutlet]="tmpl">
-      </ng-container>
-      <ng-template #tmpl>
-        Chandler Fang : Sydney AU
-      </ng-template>
+      <example-one></example-one>
+      <example-two></example-two>
+      <example-three></example-three>
     </div>
   `
 })
