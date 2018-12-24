0ac94a770d1664c61a9be75f547b41da521f4519
Chandler Fang <chandlerfang@gmail.com>
2018-12-24 22:39:35 +1100

    24-hostbinding

--- src/app/credit-card/credit-card.directive.ts
+++ src/app/credit-card/credit-card.directive.ts
@@ -1,14 +1,16 @@
-import { Directive, HostListener, ElementRef } from '@angular/core';
+import { Directive, HostListener, HostBinding, ElementRef } from '@angular/core';
 
 @Directive({
     selector: '[credit-card]'
 })
 export class CreditCardDirective {
+    @HostBinding('style.border') border: string;
+
     @HostListener('input', ['$event'])
     onKeyDown(event: KeyboardEvent) {
         const input = event.target as HTMLInputElement;
 
-        let trimmed = input.value.replace(/s+/g, '');
+        let trimmed = input.value.replace(/\s/g, '');
         if (trimmed.length  > 16) {
             trimmed = trimmed.substr(0, 16);
         }
@@ -17,8 +19,13 @@ export class CreditCardDirective {
         for (let i = 0; i < trimmed.length; i += 4) {
             numbers.push(trimmed.substr(i, 4));
         }
-        console.log(numbers);
+
         input.value = numbers.join(' ');
+
+        this.border = '';
+        if (/[^\d]+/.test(trimmed)) {
+            this.border = '1px solid red';
+        }
     }
 
     constructor(private element: ElementRef) {
